61412829248ac8228739ef10b5122201dec88cce
Gizelly Steffanny <gizelly.silva@tap4mobile.com.br>
2018-06-13 15:24:29 -0400

    Learning about wrappers

---
+++ src/fundamentos/ObjetosPrimitivos.java
@@ -0,0 +1,18 @@
+package fundamentos;
+// Wrappers
+public class ObjetosPrimitivos {
+	public static void main(String[] args) {
+		Character c = 'a';
+		Boolean bo = true;
+		
+		Byte b = 1;
+		Short s = 2;
+		Integer i;
+		Long l = 4L;
+		
+		Float f = 4.1f;
+		Double d = 6.1;
+		
+		System.out.println(bo.toString().length());
+	}
+}
