1bcb88d15f92bdba3b039758c5ae63c3304902b4
Gizelly Steffanny <gizelly.silva@tap4mobile.com.br>
2018-06-13 14:03:35 -0400

    Learning about Imports

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+import java.awt.Button;
+
+public class Import {
+	public static void main(String[] args) {
+		Double a = 2.5;
+		System.out.println(a);
+		
+		Button b;
+	}
+}
