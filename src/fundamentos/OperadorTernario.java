759b2c1a8d7f9e7af0607dc46be27d59bdbde64b
Gizelly Steffanny <gizelly.silva@tap4mobile.com.br>
2018-06-14 09:55:39 -0400

    Learning ternary operator

---
+++ src/fundamentos/OperadorTernario.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class OperadorTernario {
+	
+	public static void main(String[] args) {
+		double nota1 = 7;
+		String resultado = nota1 >= 7 ? "Aprovado" : "Recuperação";
+		System.out.println(resultado);
+	}
+}
